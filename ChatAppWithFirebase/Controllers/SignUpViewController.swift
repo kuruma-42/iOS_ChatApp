c78c3e210a7f6eaac06abfb3e49da2cd97c58ddf
kuruma-42 <perfect_yon@naver.com>
2021-04-15 00:05:24 +0900

    feat : Add ImagePicker at sign up page
    When you click profile image button then you can choose your pictures to use as a profile image.

--- ChatAppWithFirebase/Controllers/SignUpViewController.swift
+++ ChatAppWithFirebase/Controllers/SignUpViewController.swift
@@ -26,6 +26,35 @@ class SignUpViewController: UIViewController {
         
         registerButton.layer.cornerRadius = 12
         
+        profileImageButton.addTarget(self, action: #selector(tappedProfileImageButton), for: .touchUpInside)
+        
+    }
+    
+   @objc private func tappedProfileImageButton() {
+        let imagePickerController = UIImagePickerController()
+    imagePickerController.delegate = self
+    imagePickerController.allowsEditing = true
+    
+    self.present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
+//Mark: - Event after select picture
+extension SignUpViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage {
+            profileImageButton.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            profileImageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        profileImageButton.setTitle("", for: .normal)
+        profileImageButton.imageView?.contentMode = .scaleAspectFill
+        profileImageButton.contentHorizontalAlignment = .fill
+        profileImageButton.contentVerticalAlignment = .fill
+        profileImageButton.clipsToBounds = true
+        
+        dismiss(animated: true, completion: nil)
     }
 }
 
