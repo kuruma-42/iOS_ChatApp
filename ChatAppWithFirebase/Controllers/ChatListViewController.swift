6c61122d44e0723bbc8d1fecf5e75dc96da9682c
kuruma-42 <perfect_yon@naver.com>
2021-04-17 00:11:30 +0900

    feat : Add fetch User Info From Firestore function
    Function fetchUserInformation can get other users data from Firestore.
    Users data which get from FireStore bind with User model.

--- ChatAppWithFirebase/Controllers/ChatListViewController.swift
+++ ChatAppWithFirebase/Controllers/ChatListViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatListViewController: UIViewController {
     
     private let cellId = "cellId"
+    private var users = [User]()
     
     @IBOutlet weak var chatListTableView: UITableView!
     
@@ -23,10 +25,41 @@ class ChatListViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.title = "TALK"
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         
-        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
-        let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController")
-        as! SignUpViewController
-        self.present(signUpViewController, animated: true, completion: nil)
+        
+        if Auth.auth().currentUser?.uid == nil{
+            let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+            let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController")
+            as! SignUpViewController
+            signUpViewController.modalPresentationStyle = .fullScreen
+            self.present(signUpViewController, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchUserInfoFromFirestore()
+    }
+    
+    private func fetchUserInfoFromFirestore(){
+        Firestore.firestore().collection("users").getDocuments { (snapshots, err) in
+            if let err = err {
+                print("Get User Data Failed",err)
+                return
+            }
+            
+            snapshots?.documents.forEach({ (snapshot) in
+                //Mark - dic : data of users
+                let dic = snapshot.data()
+                let user = User.init(dic: dic)
+                
+                self.users.append(user)
+                self.chatListTableView.reloadData()
+                
+                self.users.forEach { (user) in
+                    print(":",user.username)
+                }
+            })
+        }
     }
 }
 
@@ -39,19 +72,20 @@ extension ChatListViewController : UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return users.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatListTableViewCell
+        
+        cell.user = users[indexPath.row]
         
         return cell
     }
     
     //MARK: - React When user tap cells
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("tapped table view ")
         let storyboard = UIStoryboard.init(name: "ChatRoom", bundle: nil)
         let chatRoomViewController = storyboard.instantiateViewController(withIdentifier: "ChatRoomViewController")
         navigationController?.pushViewController(chatRoomViewController, animated: true)
@@ -60,6 +94,18 @@ extension ChatListViewController : UITableViewDelegate,UITableViewDataSource {
 }
 
 class ChatListTableViewCell : UITableViewCell{
+    
+    var user : User? {
+        didSet {
+            if let user = user{
+                partnerLabel.text = user.username
+                //            userImageView.image = user?.profileImageurl
+                dateLabel.text = dateFormatterForDateLabel(date: user.createdAt.dateValue())
+                latestMessageLabel.text = user.email
+            }
+            
+        }
+    }
     
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var latestMessageLabel: UILabel!
@@ -71,8 +117,17 @@ class ChatListTableViewCell : UITableViewCell{
         userImageView.layer.cornerRadius = 35
     }
     
+
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
+    private func dateFormatterForDateLabel(date : Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .short
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
 }
