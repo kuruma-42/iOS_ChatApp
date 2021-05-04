7edfbc54fde94d79d52f20649cfdeaa027d91df8
kuruma-42 <perfect_yon@naver.com>
2021-05-04 23:42:35 +0900

    feat : Add UserInfo UI

---
+++ ChatAppWithFirebase/Controllers/UserInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfoViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by Yong Jun Cha on 2021/05/04.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
